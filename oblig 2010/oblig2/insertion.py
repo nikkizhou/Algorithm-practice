be9cf256881e1c50b34deab29c5190a23a423fd1
unknown <nikkizhou99@gmail.com>
2022-12-05 17:19:23 +0100

    merge two linkelist added

--- oblig 2010/oblig2/insertion.py
+++
@@ -1,9 +0,0 @@
-from countswaps import CountSwaps
-
-def sort(A:CountSwaps)-> CountSwaps:
-    for i in range (1, len(A)):
-        j = i
-        while j > 0 and A[j-1] > A[j]:
-            A.swap((j-1), j)
-            j = j-1
-    return A
